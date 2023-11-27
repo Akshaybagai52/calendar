3961ae2ab2b2662563985c20d976ad8d7fe225bf
Pradeep8051 <pradeepchauhan8051@gmail.com>
2023-11-27 08:08:49 +0530

    FIxes

--- src/app/api/getusers/route.ts
+++ src/app/api/getusers/route.ts
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import db from "../../../../lib/db";
-import { NextApiRequest, NextApiResponse } from "next";
-// import { NextApiRequest, NextApiResponse } from "next";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   const users = await db.createUsers.findMany();
   console.log(users);
   return NextResponse.json({ message: users }, { status: 200 });
