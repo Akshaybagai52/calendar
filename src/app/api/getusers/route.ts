02346af542ed43b76dbae003c4322d6c8cd0c2e6
Akshay Bagai <akshaybagai52@gmail.com>
2023-11-28 09:49:20 +0530

    fixes

--- src/app/api/getusers/route.ts
+++ src/app/api/getusers/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import db from "../../../../lib/db";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  const users = await db.user.findMany();
+  const users = await db.createUsers.findMany();
   
   console.log(users);
   return NextResponse.json({ message: users }, { status: 200 });
