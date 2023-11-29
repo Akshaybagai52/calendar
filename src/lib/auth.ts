cfa83c785bfb0c544ea2b53862faf6a5531e5d1e
Pradeep8051 <pradeepchauhan8051@gmail.com>
2023-11-29 17:00:26 +0530

    add googleauth error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -51,81 +51,10 @@ export async function loginIsRequiredServer() {
   if (!session) return redirect("/login");
 }
 
-export function loginIsRequiredClient() {
-  if (typeof window !== "undefined") {
-    const session = useSession();
-    const router = useRouter();
-    if (!session) router.push("/login");
-  }
-}
-// import prisma from "../../lib/db";
-// const bcrypt=require("bcrypt")
-// import type { NextAuthOptions } from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-
-// export const authOptions: NextAuthOptions = {
-//   pages: {
-//     signIn: "/login",
-//   },
-//   session: {
-//     strategy: "jwt",
-//   },
-//   providers: [
-//     CredentialsProvider({
-//       name: "Sign in",
-//       credentials: {
-//         email: {
-//           label: "Email",
-//           type: "email",
-//           placeholder: "example@example.com",
-//         },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials) {
-//         if (!credentials?.email || !credentials.password) {
-//           return null;
-//         }
-
-//         const user = await prisma.createUsers.findUnique({
-//           where: {
-//             email: credentials.email,
-//           },
-//         });
-
-//         if (!user || !(await bcrypt.compare(credentials.password, user.password))) {
-//           return null;
-//         }
-
-//         return {
-//           id: user.id,
-//           email: user.email,
-//           name: user.name,
-//           randomKey: "Hey cool",
-//         };
-//       },
-//     }),
-//   ],
-//   callbacks: {
-//     session: ({ session, token }) => {
-//       return {
-//         ...session,
-//         user: {
-//           ...session.user,
-//           id: token.id,
-//           randomKey: token.randomKey,
-//         },
-//       };
-//     },
-//     jwt: ({ token, user }) => {
-//       if (user) {
-//         const u = user as unknown as any;
-//         return {
-//           ...token,
-//           id: u.id,
-//           randomKey: u.randomKey,
-//         };
-//       }
-//       return token;
-//     },
-//   },
-// };
+// export function loginIsRequiredClient() {
+//   if (typeof window !== "undefined") {
+//     const session = useSession();
+//     const router = useRouter();
+//     if (!session) router.push("/login");
+//   }
+// }
